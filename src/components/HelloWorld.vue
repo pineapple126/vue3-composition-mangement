57b11fe993b4df483ba04d4f702332754bda816c
pineapple126 <zhaoq1010@foxmail.com>
2023-03-20 23:55:39 +0800

    feat: 集成状态管理工具Pinia

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+import useUserStore from '@/store/modules/user';
+
+const userStore = useUserStore();
+
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
 
 <template>
+  <div class="store">
+    <h1>在组件中您可以这样使用定义的Store</h1>
+    <h2>FirstName：{{ userStore.firstName }}</h2>
+    <h2>LastName：{{ userStore.lastName }}</h2>
+    <h2>FullName：{{ userStore.fullName() }}</h2>
+  </div>
+
   <h1>{{ msg }}</h1>
 
   <div class="card">
---
+++ src/store/modules/user.ts
@@ -0,0 +1,23 @@
+import { defineStore } from 'pinia';
+import { reactive, toRefs } from 'vue';
+
+interface IUserState {
+  firstName: string;
+  lastName: string;
+}
+
+const useUserStore = defineStore('user', () => {
+  const state = reactive<IUserState>({
+    firstName: 'Bai',
+    lastName: 'Zhantang'
+  });
+
+  const fullName = (): string => state.firstName + ' ' + state.lastName;
+
+  return {
+    ...toRefs(state),
+    fullName
+  }
+});
+
+export default useUserStore;
