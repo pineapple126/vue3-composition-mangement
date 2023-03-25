64cebff77d280c9e0b008edded815da54aa08473
pineapple126 <zhaoq1010@foxmail.com>
2023-03-26 02:02:01 +0800

    feat: 集成项目规范

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'eslint-config-prettier',
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:vue/vue3-recommended', // 使用插件支持vue3
+		'plugin:vue/vue3-essential',
+		'plugin:prettier/recommended'
+	],
+	parser: 'vue-eslint-parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		parser: '@typescript-eslint/parser'
+	},
+	plugins: ['vue', '@typescript-eslint'],
+	golbals: {
+		defineProps: 'readonly',
+		defineEmits: 'readonly',
+		defineExpose: 'readonly',
+		withDefaults: 'readonly'
+	},
+	rules: {
+		'no-console': 'warn',
+		'no-debugger': 'warn',
+		// 变量声明未使用
+		'@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '.*', args: 'none' }],
+		// 允许ts使用any
+		'@typescript-eslint/no-explicit-any': 'off'
+		// 强制使用 import 且不允许使用 require 设置off关闭检查
+		// '@typescript-eslint/no-var-requires': 'off',
+		// 对保留元素检查 vue3中v-for会自动追加key值，所以不用再强制添加key属性，所以不检查key的填写
+		// 'vue/require-v-for-key': 'off',
+		// 对于非保留(自定义)元素检查  vue3中v-for会自动追加key值，所以不用再强制添加key属性，所以不检查key的填写
+		// 'vue/valid-v-for': 'off',
+		// 添加组件命名忽略规则 vue官方默认规则是多单词驼峰来进行组件命名
+		// 'vue/multi-word-component-names': [
+		// 	'warn',
+		// 	{
+		// 		ignores: ['index'], //需要忽略的组件名
+		// 	},
+		// ],
+	}
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
-
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    eslintPlugin({
+			include: ['src/**/*.ts', 'src/**/*.vue', 'src/*.ts', 'src/*.vue'],
+		})
+  ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
+      '@': 'src'
     }
   },
   base: './',
