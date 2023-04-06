fe32b9d15983dff0b4317f625dbfe866f7ceb6ec
pineapple126 <zhaoq1010@foxmail.com>
2023-04-07 00:49:30 +0800

    feat: 集成vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue';
 import router from './router';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import i18n from '@/locales';
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -12,5 +13,6 @@ const pinia = createPinia();
 app.use(pinia);
 app.use(router);
 app.use(ElementPlus, { size: 'small', zIndex: 3000 });
+app.use(i18n);
 
 app.mount('#app');
--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -4,5 +4,8 @@ import Cookies from 'js-cookie';
 export const getSidebarStatus = () => Cookies.get(Keys.sidebarStatusKey);
 export const setSidebarStatus = (sidebarStatus: string) => Cookies.set(Keys.sidebarStatusKey, sidebarStatus);
 
+export const getLanguage = () => Cookies.get(Keys.languageKey);
+export const setLanguage = (language: string) => Cookies.set(Keys.languageKey, language);
+
 export const getSize = () => Cookies.get(Keys.sizeKey);
 export const setSize = (size: string) => Cookies.set(Keys.sizeKey, size);
